7da6824ae3d45b85a866d8163f56100a97a8744c
Kingtom404 <41447845+bennettn404@users.noreply.github.com>
2025-01-30 23:47:16 -0500

    migrate to dotnet-isolated

--- nblab-ai-fa/Function1.cs
+++ nblab-ai-fa/Function1.cs
@@ -2,24 +2,33 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Azure.AI.Vision.ImageAnalysis;
 using Azure;
 using System.Net;
 using System.Net.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace nblab_ai_fa
 {
-    public static class Function1
+    public class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+
+
+        private readonly ILogger<Function1> log;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            log = logger;
+        }
+
+        [Function("Function1")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -29,17 +38,13 @@ namespace nblab_ai_fa
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             imageUrl = imageUrl ?? data?.name;
 
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
             string responseMessage = await AnalyzeImageAsync(imageUrl); 
 
             return new OkObjectResult(responseMessage);
         }
 
 
-        static async Task<string> AnalyzeImageAsync(string imageUrl = null)
+        async Task<string> AnalyzeImageAsync(string imageUrl = null)
         {
             string endpoint = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
             string key = Environment.GetEnvironmentVariable("VISION_KEY");
